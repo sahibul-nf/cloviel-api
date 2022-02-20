3f83a46b45f0bdd513505027f959a3a9cdeee198
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-02-21 00:24:31 +0700

    chore: improve entity of event

--- event/entity.go
+++ event/entity.go
@@ -3,30 +3,30 @@ package event
 import "time"
 
 type Event struct {
-	ID                  int
-	UserID              int
-	CategoryID          int
-	CompanyID           int
-	Title               string
-	Description         string
-	Perks               string
-	Status              string
-	MemberCount         int
-	LimitedTo           int
-	Thumbnail           string
-	SignatureImage      string
-	StartDate           time.Time
-	ClosingRegistration time.Time
-	CreatedAt           time.Time
-	UpdatedAt           time.Time
+	ID                  int32     `gorm:"column:id;type:int;primaryKey;autoIncrement"`
+	UserID              int32     `gorm:"column:user_id;type:int"`
+	CategoryID          int32     `gorm:"column:category_id;type:int32"`
+	CompanyID           int32     `gorm:"column:company_id;type:int"`
+	Title               string    `gorm:"column:title;size:255"`
+	Description         string    `gorm:"column:description;size:255"`
+	Perks               string    `gorm:"column:perks;size:255"`
+	Status              string    `gorm:"column:status;size:255"`
+	MemberCount         int32     `gorm:"column:member_count;type:int"`
+	LimitedTo           int32     `gorm:"column:limited_to;type:int"`
+	Thumbnail           string    `gorm:"column:thumbnail;size:255"`
+	SignatureImage      string    `gorm:"column:signature_image;size:255"`
+	StartDate           time.Time `gorm:"column:start_date;type:timestamp"`
+	ClosingRegistration time.Time `gorm:"column:closing_registration;type:timestamp"`
+	CreatedAt           time.Time `gorm:"column:created_at;type:timestamp"`
+	UpdatedAt           time.Time `gorm:"column:updated_at;type:timestamp"`
 }
 
 type Company struct {
-	ID               int       `gorm:"column:id;type:int;primaryKey;autoIncrement" json:"id"`
-	Name             string    `gorm:"column:name;size:100" json:"name"`
-	ShortDescription string    `gorm:"column:short_description;size:255" json:"short_description"`
-	WebURL           string    `gorm:"column:web_url;size:255" json:"web_url"`
-	LogoURL          string    `gorm:"column:logo_url;size:255" json:"logo_url"`
-	CreatedAt        time.Time `gorm:"column:created_at;type:timestamp" json:"created_at"`
-	UpdatedAt        time.Time `gorm:"column:updated_at;type:timestamp" json:"updated_at"`
+	ID               int       `gorm:"column:id;type:int;primaryKey;autoIncrement"`
+	Name             string    `gorm:"column:name;size:100"`
+	ShortDescription string    `gorm:"column:short_description;size:255"`
+	WebURL           string    `gorm:"column:web_url;size:255"`
+	LogoURL          string    `gorm:"column:logo_url;size:255"`
+	CreatedAt        time.Time `gorm:"column:created_at;type:timestamp"`
+	UpdatedAt        time.Time `gorm:"column:updated_at;type:timestamp"`
 }
